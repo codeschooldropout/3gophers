27280b117d20d38742f3d6b711a14c0345c2c761
codeschooldropout <codeschooldropout@users.noreply.github.com>
2022-07-05 01:44:01 -0400

    🚧 attempt 2 - is this any better?

--- main.go
+++ main.go
@@ -15,29 +15,42 @@ import (
 )
 
 const (
-	VERSION = "0.0.1"
+	VERSION = "0.0.2"
 )
 
-// TODO Redo struct, break into smaller structs
-
+type Asset struct {
+	Exchange  string `json:"exchange"`  // 1. kucoin 2. kraken 3. coinbase - these are the ones i want to start with if possible.
+	Base      string `json:"base"`      // BTC, ETH, etc
+	Quote     string `json:"quote"`     // USD, EUR, etc
+	Timeframe string `json:"timeframe"` // 1m, 5m, 1h, 1d, 1w, 1M, 1y
+}
 type Alert struct {
-	Call            string  `json:"call"`      // entry, exit, error
-	Type            string  `json:"type"`      // long, short, take
-	Exchange        string  `json:"exchange"`  // 1. kucoin 2. kraken 3. coinbase - these are the ones i want to start with if possible.
-	Base            string  `json:"base"`      // BTC, ETH, etc
-	Quote           string  `json:"quote"`     // USD, EUR, etc
-	Price           float64 `json:"price"`     // buy high, sell low right?
-	Timeframe       string  `json:"timeframe"` // 1m 5m 15m 1h 4h 8h 1d etc
-	PNL             float64 `json:"pnl"`       // profit/loss from exit signals
-	Bars            int     `json:"bars"`      // Number of bars from entry signal to exit signal (bars x timeframe to identify corresponding signals)
-	StopLoss        float64 `json:"sl"`        // Stop loss price to set on entry signal
-	StopLossPercent float64 `json:"slp"`       // Stop loss percent to set on entry signal
-
+	Call            string  `json:"call"`               // entry, exit, error
+	Position        string  `json:"position,omitempty"` // long, short, take
+	Price           float64 `json:"price,omitempty"`    // buy high, sell low right?
+	PNL             float64 `json:"pnl,omitempty"`      // profit/loss from exit signals
+	Bars            int     `json:"bars,omitempty"`     // Number of bars from entry signal to exit signal (bars x timeframe to identify corresponding signals)
+	StopLoss        float64 `json:"sl,omitempty"`       // Stop loss price to set on entry signal
+	StopLossPercent float64 `json:"slp,omitempty"`      // Stop loss percent to set on entry signal
+	Asset           Asset   `json:"asset,omitempty"`    // Asset to trade
 }
 
 func handleRP(w http.ResponseWriter, r *http.Request) {
-	// make regex to match numbers and certain characters
-	KeepNumbersReg, err := regexp.Compile("[^0-9-.]")
+	// create regex to match numbers and certain characters
+	keepNumbersReg, err := regexp.Compile("[^0-9-.]")
+
+	// Symbols to use
+	// callIcon := "\u260E" // these icons work too (might look better as well)
+	moneyIcon := "💰"
+	stoplossIcon := "🛑"
+	enterIcon := "✅"
+	exitIcon := "❌"
+	errorIcon := "🔥"
+	longIcon := "📈"
+	shortIcon := "📉"
+
+	callIcon, positionIcon := "", ""
+
 	if err != nil {
 		logit.Log.Fatal(err)
 	}
@@ -57,91 +70,109 @@ func handleRP(w http.ResponseWriter, r *http.Request) {
 
 	// Create a new alert from the url and request body
 	// TODO make this a constructor function
-	alert := Alert{
+	asset := Asset{
 		Exchange:  strings.Split(strings.Split(r.URL.Path, "/")[2], ":")[0],
 		Base:      strings.Split(r.URL.Path, "/")[3],
 		Quote:     strings.Split(r.URL.Path, "/")[4],
 		Timeframe: strings.Split(r.URL.Path, "/")[5],
 	}
 
-	// Organize indicator data before converting to json
+	alert := Alert{}
+	alert.Asset = asset
+
 	// Seperate first line and rest of message and convert to lowercase string
 	signal, body, _ := strings.Cut(strings.ToLower(string(reqBody)), "\n")
 
-	// If signal contains MoneyBag, set Call to takeprofit
-	const MoneyBag = "💰"
-
-	// map body to usable data json
+	// body is almost well formed for json but needs external brackets
 	body = "{" + body + "}"
+	// map body so i'm not just strings.containing everything?
 	var bodyMap map[string]interface{}
 	json.Unmarshal([]byte(body), &bodyMap)
 
-	if strings.Contains(signal, MoneyBag) {
+	// Occassionally signals will register from tradingview as a signal error
+	// Identify this and return an error
+	if signal == "alert error" {
+		alert.Call = "error"
+		callIcon = errorIcon
+		positionIcon = errorIcon
+	}
 
-		// this is all broken but I am going to redo it not using signal after the take profit signal since the data source is inconsistent
-		// set alert Call to takeprofit
-		alert.Call = "EXIT"
-		alert.Type = "TAKE"
-
-		// split the body into lines
+	// EXIT SIGNALS
+	// TAKEPROFIT
+	// The takeprofit signal does not use the same format, or a well defined format like the other alert message bodies
+	// So we need to handle it separately and identify if signal is take profit
+	if strings.Contains(signal, moneyIcon) {
+		// split the body into lines for parsing
 		pnl, bars, _ := strings.Cut(body, "\n")
-		// remove non numbers from parts and cast to float64/int
-		alert.Bars, _ = strconv.Atoi(KeepNumbersReg.ReplaceAllString(bars, ""))
+		// set call and position
+		alert.Call = "exit"
+		alert.Position = "take"
+		// remove non numbers from parts and cast to correct number type
+		alert.Bars, _ = strconv.Atoi(keepNumbersReg.ReplaceAllString(bars, ""))
 		// set price from the signal message
 		alert.Price, _ = strconv.ParseFloat(strings.TrimSpace(strings.Split(signal, ":")[1]), 64)
 		// set pnl from the body
-		alert.PNL, _ = strconv.ParseFloat(KeepNumbersReg.ReplaceAllString(pnl, ""), 64)
+		alert.PNL, _ = strconv.ParseFloat(keepNumbersReg.ReplaceAllString(pnl, ""), 64)
+		// set alert icon
+		alert.StopLoss, alert.StopLossPercent = 0, 0
+		callIcon = exitIcon
+		positionIcon = moneyIcon
 
-	} else if strings.Contains(signal, "entry") {
+	}
 
-		// set Call to entry
-		alert.Call = "ENTRY"
+	if _, ok := bodyMap["exit"]; ok {
+		// EXIT LONG & SHORT & STOPLOSS
+		// set call
+		alert.Call = "exit"
+		// set long/short position
+		if bodyMap["type"].(string) == "hard long exit" {
+			alert.Position = "long"
+			positionIcon = longIcon
+		} else if bodyMap["type"].(string) == "hard short exit" {
+			alert.Position = "short"
+			positionIcon = shortIcon
+		} else if bodyMap["type"].(string) == "stop loss hit" {
+			alert.Position = "stop"
+			// change the icon if its a stop loss
+			positionIcon = stoplossIcon
+		}
 
-		// set Type to long or short
-		if strings.Contains(body, "long") {
-			alert.Type = "LONG"
-		} else if strings.Contains(body, "short") {
-			alert.Type = "SHORT"
-		} else {
-			logit.Log.Println("Unknown signal type")
+		// set price from the body
+		alert.Price, _ = strconv.ParseFloat(bodyMap["exit"].(string), 64)
+		alert.PNL, _ = strconv.ParseFloat(keepNumbersReg.ReplaceAllString(bodyMap["pnl"].(string), ""), 64)
+		alert.Bars, _ = strconv.Atoi(bodyMap["traded bars"].(string))
+		// set the icon
+		callIcon = exitIcon
+
+	} else if _, ok := bodyMap["enter"]; ok {
+		// ENTER LONG & SHORT
+		// set call
+		alert.Call = "enter"
+		// set long/short position
+		if bodyMap["type"].(string) == "long signal" {
+			alert.Position = "long"
+			positionIcon = longIcon
+		} else if bodyMap["type"].(string) == "short signal" {
+			alert.Position = "short"
+			positionIcon = shortIcon
 		}
 
 		// set price from body map
 		alert.Price, _ = strconv.ParseFloat(bodyMap["enter"].(string), 64)
 		alert.StopLoss, _ = strconv.ParseFloat(bodyMap["sl"].(string), 64)
 		alert.StopLossPercent, _ = strconv.ParseFloat(bodyMap["slp"].(string), 64)
-
-		// fmt.Println("bodyMap: ", bodyMap)
-
-	} else if strings.Contains(signal, "exit") {
-		// do other exit Call stuff
-		alert.Call = "EXIT"
-
-		// set Type to long or short
-		if strings.Contains(body, "long") {
-			alert.Type = "LONG"
-		} else if strings.Contains(body, "short") {
-			alert.Type = "SHORT"
-		} else if strings.Contains(body, "stop") {
-			alert.Type = "STOP"
-		} else {
-			logit.Log.Println("Unknown signal type")
-		}
-
-	} else if strings.Contains(signal, "error") {
-		// If signal contains error, set Call to error
-		alert.Call = "ERROR"
+		//set the icon
+		callIcon = enterIcon
 	}
 
 	// Convert alert to json
 	alertBytes, _ := json.Marshal(alert)
-
+	// return json to caller (this isn't needed except for testing or if i send signals from things that are not tradingview
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(alertBytes))
 
-	//fmt.Printf("%#v\n", body)
-	//fmt.Printf("%#v\n", signal)
-
-	fmt.Printf("%+v\n", alert)
+	// print alert to console
+	logit.Log.Printf("%s %s Alert: %s", callIcon, positionIcon, string(alertBytes))
 }
 
 func handleJSON(w http.ResponseWriter, r *http.Request) {
